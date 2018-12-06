1b4f4f741b4a1e1c412250db8d9b344eb93e47de
frank870622 <frank870622@gmail.com>
2018-12-06 14:02:58 +0800

    asdad

--- app.py
+++ app.py
@@ -253,4 +253,4 @@ def webhook_handler():
 #    return static_file('fsm.png', root='./', mimetype='image/png')
 
 if __name__ == "__main__":
-    run(host="0.0.0.0", port=PORT, debug=True, reloader=True)
+    app.run(host="0.0.0.0", port=PORT, debug=True, reloader=True)
